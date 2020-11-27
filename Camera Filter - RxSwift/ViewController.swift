85fbdd1b3801e37de3aa0c22c067abd321b63fec
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-27 20:13:28 -0300

    Initial Commit

---
+++ Camera Filter - RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera Filter - RxSwift
+//
+//  Created by Ezequiel Parada Beltran on 27/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
