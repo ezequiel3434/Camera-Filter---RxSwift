068c6f2c49b9dbf1370243eb325bc389d3f7395c
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-27 21:01:18 -0300

    setting up the user interface

---
+++ Camera Filter - RxSwift/PhotosCollectionViewControllers.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewControllers.swift
+//  Camera Filter - RxSwift
+//
+//  Created by Ezequiel Parada Beltran on 27/11/2020.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
