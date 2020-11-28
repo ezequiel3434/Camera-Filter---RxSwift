8bf9429af14f77f0b3ff71451f31a1599a7e46ee
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-28 14:07:36 -0300

    displaying photos from photo library

---
+++ Camera Filter - RxSwift/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Camera Filter - RxSwift
+//
+//  Created by Ezequiel Parada Beltran on 28/11/2020.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
