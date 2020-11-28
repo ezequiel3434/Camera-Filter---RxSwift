61d372712f38cb84ceb8c55bb68c057c5f45db4c
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-28 14:36:56 -0300

    subscribing and displaying the selected photo

--- Camera Filter - RxSwift/ViewController.swift
+++ Camera Filter - RxSwift/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navC = segue.destination as? UINavigationController,
+              let photosCVC = navC.viewControllers.first as? PhotosCollectionViewController else {
+            fatalError("Segue destination is not found")
+        }
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            
+            guard let self = self else {return}
+            self.photoImageView.image = photo
+            
+        }).disposed(by: disposeBag)
     }
 
 
