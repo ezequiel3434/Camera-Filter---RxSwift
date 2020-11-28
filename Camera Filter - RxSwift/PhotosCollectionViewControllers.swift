5bf8e0d3a1206b34857a3d68417e667bac5e2096
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-28 12:54:00 -0300

    Requesting Permission to access photo library

--- Camera Filter - RxSwift/PhotosCollectionViewControllers.swift
+++ Camera Filter - RxSwift/PhotosCollectionViewControllers.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func populatePhotos(){
+        PHPhotoLibrary.requestAuthorization{ status in
+            if status == .authorized {
+                // access the photos from photo library
+                
+            }
+        }
+    }
 }
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Camera Filter - RxSwift' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Camera Filter - RxSwift
+	pod 'RxSwift', '6.0.0-rc.1'
+    
+
+end
