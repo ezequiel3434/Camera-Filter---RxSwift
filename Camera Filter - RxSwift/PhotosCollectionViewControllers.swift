063455af78714de9ecbc403a997f934b1ce36012
ezequiel3434 <ezequiel3434@gmail.com>
2020-11-28 13:06:02 -0300

    fetching all photo from photo library

--- Camera Filter - RxSwift/PhotosCollectionViewControllers.swift
+++ Camera Filter - RxSwift/PhotosCollectionViewControllers.swift
@@ -11,16 +11,28 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         populatePhotos()
     }
     
     private func populatePhotos(){
-        PHPhotoLibrary.requestAuthorization{ status in
+        PHPhotoLibrary.requestAuthorization{ [weak self] status in
+            
+            guard let self = self else { return }
+            
             if status == .authorized {
                 // access the photos from photo library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
                 
+                assets.enumerateObjects { (object, count, stop) in
+                    self.images.append(object)
+                }
+                
+                self.images.reverse()
+                self.collectionView.reloadData()
             }
         }
     }
